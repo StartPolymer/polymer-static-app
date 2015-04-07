eb95c4185fbf1d26245aa8e3110d19665ddeba1a
Josef Jezek <josef@jezek.co.cz>
2015-04-07 18:37:05 +0200

    Update metalsmith task - add tags, validating metadata, writing metadata to json file

--- gulp/tasks/metalsmith.js
+++ gulp/tasks/metalsmith.js
@@ -2,98 +2,149 @@
 
 // Metalsmith
 module.exports = function(gulp, plugins, config) { return function() {
-  var assign        = require('lodash.assign');
-  var metalsmith    = require('gulpsmith')();
+  //var assign        = require('lodash.assign');
+  var metalsmith    = require('metalsmith');
   var moment        = require('moment');
 
+  //var api           = require('metalsmith-json-api');
   var branch        = require('metalsmith-branch');
   var collections   = require('metalsmith-collections');
+  //var define        = require('metalsmith-define');
+  var each          = require('metalsmith-each');
   var excerpts      = require('metalsmith-excerpts');
   var markdown      = require('metalsmith-markdownit');
-  var metadata      = require('metalsmith-metadata');
+  //var metadata      = require('metalsmith-metadata');
   var permalinks    = require('metalsmith-permalinks');
+  var tags          = require('metalsmith-tags');
   var templates     = require('metalsmith-templates');
+  var validate      = require('metalsmith-validate');
+  var writemetadata = require('metalsmith-writemetadata');
   //var ignore        = require('metalsmith-ignore');
 
+  var dateBuild = new Date();
 
-  // filter out files with front matter
-  //var fmFilter = $.filter('**/*.{html,md,htb}');
+  return metalsmith('./')
+    .source('app/content')
+    .destination('dist')
 
-  // Add a template key to posts (if there is none), so we’ll need to filter out our posts first.
-  var findTemplate = function(config) {
-    var pattern = new RegExp(config.pattern);
-
-    return function(files, metalsmith, done) {
-      for (var file in files) {
-        if (pattern.test(file)) {
-          var _f = files[file];
-          if (!_f.template) {
-            _f.template = config.templateName;
+    .use(each(function(file, filename) {
+      file.date_build = dateBuild;
+      var data = filename.split('/');
+      data = data[data.length - 1].split('.');
+      file.name = data[0];
+      file.lang = data[1];
+    }))
+    //.use(metadata({
+    //  config: config.metalsmith.configFile
+    //}))
+    .use(validate([{
+      pattern: 'pages/**/*.md',
+      metadata: {
+        title: true,
+        slug: true,
+        template: {
+          default: 'page.jade'
+        }
+      }
+    }, {
+      pattern: 'posts/**/*.md',
+      metadata: {
+        title: true,
+        slug: true,
+        tags: {
+          exists: true,
+          type: 'Array'
+        },
+        date: {
+          exists: true,
+          type: 'Date'
+        },
+        date_modify: {
+          exists: true,
+          type: 'Date'
+        },
+        author: true,
+        author_url: true,
+        template: {
+          default: 'post.jade'
+        }
+      }
+    }, {
+      // pattern defaults to '**/*'
+      metadata: {
+        template: {
+          pattern: function(value) {
+            return value.match(/\.jade$/);
           }
         }
       }
+    }]))
 
-      done();
-    };
-  };
-
-  metalsmith
-    .use(metadata({
-      config: config.metalsmith.configFile
+    .use(tags({
+      handle: 'tags',
+      path:'tag/:tag/index.html',
+      template:'tag.jade',
+      sortBy: 'date',
+      reverse: true
     }))
 
-    .use(findTemplate({
-      pattern: 'posts',
-      templateName: 'post.jade'
-    }))
+    //.use(findTemplate({
+    //  pattern: 'posts',
+    //  templateName: 'post.jade'
+    //}))
     .use(markdown())
     .use(excerpts())
     .use(collections({
       pages: {
-        pattern: 'pages/*.html'
+        pattern: 'pages/**/*.html'
       },
       posts: {
-        pattern: 'posts/*.html',
-        sortBy: 'date',
-        reverse: true
+        pattern: 'posts/**/*.html'
+        //sortBy: 'date',
+        //reverse: true
       }
     }))
     .use(templates({
       engine: 'jade',
-      directory: './app/templates',
+      directory: 'app/templates',
       moment: moment
     }))
-    //.use(ignore([
-    //  'templates/**/*'
-    //]))
-
-    .use(branch('posts/*')
+    .use(branch('home/**/*.html')
       .use(permalinks({
-        pattern: ':slug-:date',
+        pattern: ':lang'
+      }))
+    )
+    .use(branch('pages/**/*.html')
+      .use(permalinks({
+        pattern: ':lang/:slug'
+      }))
+    )
+    .use(branch('posts/**/*.html')
+      .use(permalinks({
+        pattern: ':lang/:slug-:date',
         date: 'YYYYMM'
       }))
-    );
-
-
-  return gulp.src([
-      'app/content/**/*.md',
-      config.metalsmith.configFile
-    ])
-
-    //.pipe(fmFilter)
-
-    // Grab files with front matter and assign them as a property so metalsmith will find it
-    .pipe(plugins.frontMatter({
-      property: 'frontMatter'
-    })).on('data', function(file) {
-      assign(file, file.frontMatter);
-      delete file.frontMatter;
-    })
-
-    .pipe(metalsmith)
-    .pipe(gulp.dest('./dist'));
-
-  // remove the filter (back to everything in /src) and let metalsmith do its thing
-  //.pipe(fmFilter.restore())
+    )
+    .use(writemetadata({
+      pattern: ['**/*.html'],
+      ignorekeys: ['next', 'previous'],
+      bufferencoding: 'utf8',
+      collections: {
+        posts: {
+          output: {
+            path: 'posts.json',
+            asObject: true,
+            metadata: {
+              type: 'list'
+            }
+          },
+          ignorekeys: ['contents', 'history', 'next', 'previous', 'stats']
+        }
+      }
+    }))
+    //.use(api())
+    .build(function(err, files) {
+      if (err) { console.log(err); }
+    });
 
 };};
